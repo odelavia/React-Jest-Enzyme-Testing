e8be8aae187cfb9512ec3e0b9e75faf541d8165a
Oliver De La Via <odelavia@gmail.com>
2018-06-25 22:23:55 -0400

    set up all tests for  CommentBox test suite

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+ state = { comment: '' };
+
+ handleChange = event => {
+  this.setState({ comment: event.target.value })
+ }
+
+ handleSubmit = event => {
+  this.setState({ comment: '' })
+ }
+
   render() {
     return (
-      <div>
-        CommentBox
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea onChange={this.handleChange} value={this.state.comment}/>
+        <div>
+         <button>Submit Comment</button>
+        </div>
+      </form>
     );
   }
 }
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let component;
+
+beforeEach(() => {
+  component = shallow(<CommentBox />);
+})
+
+afterEach(() => {
+  component.unmount();
+})
+
+it('has a text area and a button', () => {
+  expect(component.find('textarea').length).toEqual(1)
+  expect(component.find('button').length).toEqual(1)
+});
+
+describe('the text area', () => {
+  beforeEach(() => {
+    component.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    component.update();
+  });
+
+  it('has a text area that users can type in', () => {
+    expect(component.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('when form is submitted, text area gets emptied', () => {
+    expect(component.find('textarea').prop('value')).toEqual('new comment');
+    component.find('form').simulate('submit');
+    component.update();
+    expect(component.find('textarea').prop('value')).toEqual('');
+  });
+});
